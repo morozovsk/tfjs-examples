ff6af7e47ffa70345cf5c147676b486edbf754bd
Vladimir Goncharov <morozovsk@gmail.com>
2020-09-18 01:26:35 +0300

    _

--- README.md
+++ README.md
@@ -7,9 +7,15 @@ installation:
 * git clone https://github.com/morozovsk/tfjs-examples.git && cd tfjs-examples
 * npm install
 
+update:
+* cd tfjs-examples && git pull
+* npm install
+
 using:
 * nodejs face_comparison.js
 * nodejs face_recognition.js
+* nodejs face_extractor.js
+* nodejs face_extractor_dir.js
 
 dependencies installation:
 * apt update && apt install -y git curl wget nodejs npm make g++
---
+++ face_extractor_dir.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const faceapi = require('face-api.js');
+const tf = require('@tensorflow/tfjs-node');
+const sharp = require('sharp');
+
+const start = async function() {
+    await faceapi.nets.ssdMobilenetv1.loadFromDisk('./weights');
+    await faceapi.nets.faceLandmark68Net.loadFromDisk('./weights');
+    await faceapi.nets.faceRecognitionNet.loadFromDisk('./weights');
+
+
+    var files = fs.readdirSync('images/');
+
+    for (var j in files) {
+        var file = 'images/' + files[j];
+
+        if (!fs.statSync(file).isFile()) continue;
+
+        img = tf.node.decodeImage(fs.readFileSync(file));
+
+        // get all faces with descriptors
+        var detections = await faceapi.detectAllFaces(img, new faceapi.SsdMobilenetv1Options({minConfidence: 0.5})).withFaceLandmarks().withFaceDescriptors();
+
+        for (var i in detections) {
+            var box = detections[i].detection.box;
+            //console.log(box);
+            var img = sharp(file);
+            var metadata = await img.metadata();
+            //console.log(metadata);
+            var faceBox = {
+                width: Math.round(box.width*2 + box.x < metadata.width ? box.width * 2 : metadata.width - box.x),
+                height: Math.round(box.height*2 + box.y < metadata.height ? box.height * 2 : metadata.height - box.y),
+                left: Math.round(box.x - box.width/2 > 0 ? box.x - box.width/2 : 0),
+                top: Math.round(box.y - box.height/2 > 0 ? box.y - box.height/2 : 0)
+            };
+            img.extract(faceBox).toFile(`images/faces/${files[j].split('.')[0]}_${i}.png`);
+
+            //box = detections[i].alignedRect.box;
+            //console.log(box);
+        }
+    }
+    
+};
+
+start();
